bafdd1e159c72b26f4516753481290ebec573826
Liubov Krivtsova <avostvirk@gmail.com>
2023-05-01 10:33:19 +0300

    feat: add deleting a user

--- methods/delete-request.js
+++ methods/delete-request.js
@@ -1,1 +1,36 @@
-module.exports = (req, res) => {};
+const writeToFile = require('../util/write-to-file');
+
+module.exports = (req, res) => {
+  const baseUrl = req.url.substring(0, req.url.lastIndexOf('/') + 1);
+  const id = req.url.split('/')[3]; // TODO: 3
+  const regexV4 = new RegExp(
+    /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/
+  );
+
+  if (!regexV4.test(id)) {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({
+        title: 'Validation Failed',
+        message: 'UUID is not valid',
+      })
+    );
+  } else if (baseUrl === '/api/users/' && regexV4.test(id)) {
+    const index = req.users.findIndex((user) => user.id === id);
+
+    if (index === -1) {
+      res.statusCode = 404;
+      res.write(
+        JSON.stringify({
+          title: 'Not Found',
+          message: 'User not found',
+        })
+      );
+    } else {
+      req.users.splice(index, 1);
+      writeToFile(req.users);
+      res.writeHead(204, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(req.users));
+    }
+  }
+};
